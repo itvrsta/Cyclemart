0a55637e7cb298b8e938dfa9b4430dff3432712d
Vriesta <vriesta.ita888@gmail.com>
2023-11-28 08:29:03 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CYCLEMART</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style></style>
+        
+         @vite('resources/css/app.css')
+    </head>
+    <body>
+      <div class="avatar">
+        <div class="w-30 flex justify-center rounded">
+          <img src="/img/logo.svg" />
+        </div>
+      </div>
+      <nav class="bg-[#8897AA]">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-6">
+          <div class="relative flex h-4 items-center justify-center">
+            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            </div>
+          </div>
+        </div>
+              </button>
+              <div class="hidden sm:ml-5 sm:block">
+                <div class="flex justify-center">
+                  <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                  <a href="#Home" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-12 py-2 text-xl font-light">Home</a>
+                  <a href="#Cyclemart" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-12 py-2 text-xl font-light">Cyclemart</a>
+                  <a href="#Tentang kami" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-12 py-2 text-xl font-light">Tentang kami</a>
+                  <a href="/halaman" class="text-white hover:bg-gray-700 hover:text-white rounded-md px-12 py-2 text-xl font-light">Register</a>
+                </div>
+              </div>
+            <nav class="bg-[#F4EDE5]">
+                <div class="mx-auto max-w-full px-2 sm:px-6 lg:px-8">
+                  <div class="relative flex h-0 items-center justify-center">
+                    <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    </div>
+                  </div>
+                </div>  
+                <div class="bg" Id="Home">
+                  <div class="bg-[length:200px_100px] w-full">
+                    <img class="w-full" src="/img/page 1.svg" />
+                  </div>
+                  </div>
+                  <div class="bg" Id="Cyclemart">
+                    <div class="bg-[length:200px_100px] w-full">
+                      <img class="w-full" src="/img/page 2.svg" />
+                    </div>
+                    <br>
+                      <div class="bg" Id="Tentang kami">
+                        <div class="bg-[length:200px_100px] w-full">
+                          <img class="w-full" src="/img/footer.svg" />
+                        </div>
+                  
+                    
+
+
+                      
+    </body>
+</html>
